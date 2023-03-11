74aec3c9f4260bdb89b54629a7582057c5b23989
Adam Dia <43157907+dooplu@users.noreply.github.com>
2023-03-11 11:40:26 -0500

    Update uart.py

--- Client Python/uart.py
+++ Client Python/uart.py
@@ -1,15 +1,29 @@
 import serial
 import clientClass
+import threading
 
-client = clientClass.TCPClient("10.0.0.106", 9000)
-ser = serial.Serial('/dev/serial0', 115200)
+def start_UART_relay(_serial, _client):
+        while True:
+            if _serial.in_waiting <= 0: continue
+            message = _serial.readline().decode()
+            _client.send(message)
+
+serial_mcu = serial.Serial('/dev/ttyAMA0', 115200, timeout=1)
+debug_mcu = serial.Serial('/dev/ttyAMA1', 115200, timeout=1)
+
+thread_alive = True
+
+client = clientClass.UART_TCPClient("192.168.20.172", 9000, serial_mcu)
+
+## Uncomment for third serial connection, used for debug
+# debug = serial.Serial('/dev/serial0', 115200)
+# debug_client = clientClass.UART_TCPClient("10.0.0.106", 9000, debug)
 
 client.connect()
-try:
-    while True:
-        message = ser.readline()
-        #print(message.decode())
-        client.send(message)
-except KeyboardInterrupt:
-    client.disconnect()
-    ser.close()
+
+receiveThread_Serial = threading.Thread(target=start_UART_relay, args = (serial_mcu, client))
+receiveThread_Debug = threading.Thread(target=start_UART_relay, args = (debug_mcu, client))
+
+receiveThread_Serial.start()
+receiveThread_Debug.start()
+
